109c1df76b02362a718690e0e1a9cfd507c1007d
hyoni0817 <hyoni0817@naver.com>
2023-06-22 23:22:07 +0900

    Feat: navbar에 협업 메뉴 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,6 +77,7 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/collaboration', label: '협업', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
